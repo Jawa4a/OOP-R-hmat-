459131204bb1c84432bbfdb1a7af3e67c3de4dcc
Robin Murumets <99771771+robinmurumets@users.noreply.github.com>
2024-04-07 18:08:53 +0000

    Tegin uue kasutaja tervituse ja sisselogimise / registreerimise

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -10,9 +10,46 @@ public class Main {
 
     public static void main(String[] args) throws IOException{
         // tuleb teha loogika, mis tervitab kasutajat, annab valiku teha uus konto või logida sisse olemasolevaga, kutsub vastava meetodi jne jne
-        LoginOrSignup seanss = new LoginOrSignup();
-        LoginSignupResponse userInfo = seanss.signOrRegister();
-        if (userInfo!=null)
+
+        System.out.println("Tere tulemast! Kas soovid logida sisse või registreeruda?");
+        System.out.println("1. Logi sisse");
+        System.out.println("2. Registreeru");
+    
+
+        int valik = scanner.nextInt();
+        LoginOrSignup session = new LoginOrSignup();
+        LoginSignupResponse kasutajaInfo = null;
+
+        
+        switch (valik) {
+            case 1:
+                kasutajaInfo = session.logIn(); 
+                if (kasutajaInfo != null) {
+                    System.out.println("Oled sisse logitud!");
+                } else {
+                    System.out.println("Sisselogimine ebaõnnestus");
+                }
+                break;
+            case 2:
+                
+                kasutajaInfo = session.signup(); 
+                if (kasutajaInfo != null) {
+                    System.out.println("Registreerumine õnnestus!");
+                } else {
+                    System.out.println("Registreerumine ebaõnnestus");
+                }
+                break;
+            default:
+                System.out.println("Tundmatu valik, palun proovi uuesti.");
+                break;
+        }
+        
+
+
+        //LoginOrSignup seanss = new LoginOrSignup();
+        //LoginSignupResponse kasutajaInfo = seanss.signOrRegister();
+
+        if (kasutajaInfo!=null)
         {
             System.out.println("\nViimased postitused:");
             Activity activity = new Activity();
